542422ce79ea824a49c86d8689d52d90fcc696f3
guyue <lipeng@xinmei365.com>
2016-07-26 14:23:03 +0800

    Initial commit

---
+++ src/com/youmeek/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.youmeek.demo;
+
+/**
+ * Created by xinmei365 on 16/7/26.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello world");
+    }
+}
